c5b700b3b6d72aede0146b6b69fca75ca6e40df9
HeyMilkshake <38050116+msmilkshake@users.noreply.github.com>
2022-10-24 14:11:59 +0100

    Update HelloGoodbye.java

--- HelloGoodbye.java
+++ HelloGoodbye.java
@@ -2,5 +2,15 @@ public class HelloGoodbye {
     public static void main(String[] args) {
         System.out.println("Hello " + args[0] + " and " + args[1] + ".");
         System.out.println("Goodbye " + args[1] + " and " + args[0] + ".");
+        
+        System.out.print("Insert time in seconds: ");
+		int time = Integer.parseInt(scanner.nextLine());
+		System.out.print("Insert distance in kilometers: ");
+		double distance = Double.parseDouble(scanner.nextLine());
+		int minutes = (int)(time / distance / 60);
+		int seconds = (int)(((time / distance / 60) - minutes) * 60);
+		System.out.printf("%02d:%02d/km", minutes, seconds);
+		System.out.print("\nPress ENTER to exit");
+		scanner.nextLine();
     }
 }
